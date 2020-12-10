4344b323ad43d90793a0553fd8375db2d8b031d9
ksy <zmdkdkt321@gmail.com>
2020-12-10 17:21:35 +0900

    feat custom recent

--- controller/data/custom.js
+++ controller/data/custom.js
@@ -8,6 +8,49 @@ const model = {
 
 module.exports = {
   //새로운 부위 추가
+  get: async (req, res) => {
+    const sess = req.session;
+    //세션의 확인 유무
+    if (sess.userid) {
+      try {
+        body_parts = await body_part.findAll({
+          where: {
+            user_id: sess.userid,
+          },
+        });
+        body_parts = body_parts.slice(7, body_parts.length);
+        let result = [];
+        for (let i of body_parts) {
+          let custompart = {
+            part_name: i.body_part,
+            value: null,
+          };
+          //세션과 동일한 유저가쓴 특정 부위의 데이터 다 가져오기
+          let recent = await body_data.findAll({
+            where: {
+              body_part_id: i.id,
+              user_id: sess.userid,
+            },
+          });
+          //결과값이 존재한다면 result에 최근값 한개 추가하기
+          if (recent.length === 0) {
+            custompart.value = null;
+          } else {
+            custompart.value = recent.pop().value;
+          }
+          result.push(custompart);
+        }
+        res.status(200).send(result);
+      } catch (e) {
+        //try문에서 오류가 생겼을시
+        console.log(e);
+        res.status(500).send("정보를 가져오는데 실패했습니다");
+      }
+    } else {
+      //세션이 없을시
+      res.status(404).send("세션이 없습니다");
+    }
+  },
   post: (req, res) => {
     const sess = req.session;
     const { part_name } = req.body;
