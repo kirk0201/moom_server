aa8a88c54c04a6d9ccf6bcf440712868586b0b0d
ksy <zmdkdkt321@gmail.com>
2020-12-13 20:45:04 +0900

    fix/write,allbasic,seed

--- controller/data/allbasic.js
+++ controller/data/allbasic.js
@@ -16,6 +16,7 @@ module.exports = {
           "weight",
         ];
         let result = {};
+        result.schedule = ["19990101"];
         for (let i of basic) {
           // 특정 부위의 id값 찾기
           let { id } = await body_part.findOne({
@@ -32,17 +33,48 @@ module.exports = {
               body_part_id: id,
             },
           });
-          //데이터 에서 값만 가져오기
-          if(i === 'body_fat'){
-            result.schedule = body_datas.map((i) => {
-                return i.schedule.slice(5);
-              });
+          //제일 최신 정보가 있는 부위의 최근 12일이 result.schedule에 담김
+          if (
+            body_datas[body_datas.length - 1].schedule >
+            result.schedule[result.schedule.length - 1]
+          ) {
+            result.schedule = body_datas.slice(-12).map((i) => {
+              return i.schedule;
+            });
           }
-          result[i] = body_datas.map((i) => {
-            return i.value;
-          });
         }
-
+        console.log("result.schedule" + result.schedule);
+        for (let i of basic) {
+          // 특정 부위의 id값 찾기
+          let { id } = await body_part.findOne({
+            where: {
+              user_id: sess.userid,
+              body_part: i,
+            },
+            attributes: ["id"],
+          });
+          // 특정 id에 해당하는 모든 데이터 가져오기
+          let body_datas = await body_data.findAll({
+            where: {
+              user_id: sess.userid,
+              body_part_id: id,
+            },
+          });
+          let data = {};
+          for(let value of body_datas){
+            data[value.schedule] = value.value
+          }
+          let values = [];
+          for (let day of result.schedule) {
+            if (data[day]) {
+              values.push(data[day]);
+            } else {
+              values.push(null);
+            }
+          }
+          result[i] = values;
+        }
+        console.log("result" + result);
         res.status(200).send(result);
       } catch (e) {
         console.log(e);
