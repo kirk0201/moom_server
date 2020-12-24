b0a67feb7962f32aa89eddc703b51d405d5d1d5b
unknown <mine6546@gmail.com>
2020-12-25 01:18:57 +0900

    heroku test2

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(
 );
 //------------------------ 여기서 부터 배포 전용 입니다. --------------------------- ctrl + shift + a
 // TODO: 배포용 포트
-const port = 443;
+const port = process.env.PORT;
 
 var client = redis.createClient(6379, "localhost");
 
@@ -55,16 +55,16 @@ app.use("/user", userRouter);
 // TODO:HTTPS-PROTOCAL (배포 용)
 // HTTPS설정을 위한 인증서 옵션
 // 임의로 인증서 위치를 옮기거나 복사하지말고 원본 위치를 사용할것
-let sslOption = {
-  ca: fs.readFileSync("/etc/letsencrypt/live/m00m.cf/fullchain.pem"),
-  key: fs.readFileSync("/etc/letsencrypt/live/m00m.cf/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/m00m.cf/cert.pem"),
-};
+// let sslOption = {
+//   ca: fs.readFileSync("/etc/letsencrypt/live/m00m.cf/fullchain.pem"),
+//   key: fs.readFileSync("/etc/letsencrypt/live/m00m.cf/privkey.pem"),
+//   cert: fs.readFileSync("/etc/letsencrypt/live/m00m.cf/cert.pem"),
+// };
 //<---- 서버 생성 express의 app.listen()과 같은 로직이다 공식문서에 나와있으니 참고   ---->
 // http는 참고용으로 남겨둠 배포, 로컬에서 주석처리할것
-// http.createServer(app).listen(80);
+http.createServer(app).listen(port);
 // HTTPS는 인증서가 발급된 상태에서만 만들수 있기 때문에 해당 옵션을 추가해야함
-https.createServer(sslOption, app).listen(port);
+// https.createServer(sslOption, app).listen(port);
 
 app.get("/", (req, res) => {
   res.status(200).send("Connect Server!!");
